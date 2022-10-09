30f634b6f35453e566c3febcfd27c7c667f997c1
Desanta88 <101564257+Desanta88@users.noreply.github.com>
2022-10-09 20:56:39 +0200

    fine programma?

--- Visualizzazione_rapida/Form1.Designer.cs
+++ Visualizzazione_rapida/Form1.Designer.cs
@@ -28,12 +28,83 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(207, 202);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(134, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Cerca primo edificio";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(207, 132);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Scrivi il nome del comune";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(179, 163);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(183, 23);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(415, 166);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Edificio:";
+            this.label2.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(495, 166);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "label3";
+            this.label3.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(892, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Button button1;
+        private Label label1;
+        private TextBox textBox1;
+        private Label label2;
+        private Label label3;
     }
 }
--- Visualizzazione_rapida/Form1.cs
+++ Visualizzazione_rapida/Form1.cs
@@ -1,10 +1,121 @@
+using System.Text;
 namespace Visualizzazione_rapida
 {
     public partial class Form1 : Form
     {
+        public string filename = @"./veneto_verona.csv";
+        public string line="",nc="",ne="",edificio="";
+        public int nr,posi;
+        //creare una funziona che conta il numero di byte totale così che poi se faccio la ricerca e mi si posiziona a metà del file allora se divido il numero totale di byte e divido per pos,allora ottengo la riga
+        //creare una funzione che attraverso la ricerca dicotomica,posiziona il puntatore nella i a metà e di conseguenza,se non è quello spostati indietro o avanti
+        public int RicercaDicotomica( ref byte[] b,int r)
+        {
+            int pos = -1, m = 0, i = 0,j=r, sc = 0 ;
+            string line="",co="";
+            m = r / 2;
+            do
+            {
+                var f = new FileStream(filename, FileMode.Open, FileAccess.Read);
+                BinaryReader n = new BinaryReader(f);
+                f.Seek((m * 528)-528, SeekOrigin.Current);
+                b = n.ReadBytes(528);
+                line = Encoding.ASCII.GetString(b, 0, b.Length);
+                co=FromStringComune(line, ";");
+                sc = string.Compare(co, textBox1.Text);
+                if (co == textBox1.Text)
+                {
+                    pos = m;
+                }
+                else if (sc < 0)
+                {
+                    i = m++;
+                }
+                else if(sc>0)
+                {
+                    j = m--;
+                }
+            } while ((m>=0 && m<=r) && pos == -1);
+
+            return pos;
+
+        }
+        public int contaByte(byte[] b)
+        {
+            int fileLenght = 528;
+            var f = new FileStream(filename, FileMode.Open, FileAccess.Read);
+            BinaryReader r = new BinaryReader(f);
+            f.Seek(0, SeekOrigin.Begin);
+            for(int i = 0; i < 2592; i++)
+            {
+                b = r.ReadBytes(fileLenght);
+                fileLenght = fileLenght+528;
+            }
+           return fileLenght;
+        }
+        public int contaPos(byte[] b)
+        {
+            int lineTOT;
+            int lineLenght = 528;
+            var f = new FileStream(filename, FileMode.Open, FileAccess.Read);
+            int lineBytes = Convert.ToInt32(f.Length);
+            BinaryReader r = new BinaryReader(f);
+            f.Seek(0, SeekOrigin.Begin);
+            lineTOT = lineBytes / lineLenght;
+            return lineTOT;
+
+        }
+
+        public string FromStringComune(string s,string sep = ";")
+        {
+            string co;
+            string[] record=s.Split(sep);
+            co = record[0];
+            return co;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public string FromStringEdi(string s, string sep = ";")
+        {
+            string co;
+            string[] record = s.Split(sep);
+            co = record[7];
+            return co;
+        }
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            /*int recordLenght = 528;
+            var f = new FileStream(filename, FileMode.Open, FileAccess.Read);
+            BinaryReader r = new BinaryReader(f);
+            byte[] br;
+            f.Seek(0, SeekOrigin.Begin);
+            br =r.ReadBytes(recordLenght);
+            line = Encoding.ASCII.GetString(br, 0, br.Length);
+            nc=FromStringComune(line, ";");
+            textBox1.Text = nc;
+            ne = FromStringEdi(line, ";");
+            textBox2.Text = ne;*/
+            
+            byte[] br = {0};
+            //fl=contaByte(br);
+            nr = contaPos(br)+1;
+            posi = RicercaDicotomica(ref br, nr);
+            line = Encoding.ASCII.GetString(br, 0, br.Length);
+            edificio = FromStringEdi(line, ";");
+            label2.Visible = true;
+            label3.Visible = true;
+            label3.Text = edificio;
+            //textBox1.Text = posi.ToString();
+
+
         }
     }
 }
